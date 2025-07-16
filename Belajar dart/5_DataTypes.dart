acff6058ebeda9b3df0c6954c40722703453a44c
rizalsuryana <rizal.suryn@gmail.com>
2025-07-16 23:41:44 +0700

    data types on dart

---
+++ Belajar dart/5_DataTypes.dart
@@ -0,0 +1,23 @@
+// ==================== Tipe Data di Dart ==============================|
+// Tipe   | Deskripsi                     | Contoh                      |
+// -------|-------------------------------|-----------------------------|
+// int    | Bilangan bulat                | 5, -7, 0                    |
+// double | Bilangan desimal              | 3.14, 18.0, -12.12          |
+// num    | Bilangan bulat & desimal      |                             |
+//          (parent dari int & double)      5, 3.14, -99.00             |
+// bool   | Boolean (nilai benar/salah)   | true, false                 |
+// String | Teks (kumpulan karakter)      | 'Dicoding', 'Y', ''         |
+// List   | Daftar nilai (array)          | [1, 2, 3], ['a', 'b', 'c']  |
+// Map    | Pasangan key-value            | {'x': 4, 'y': 10}           |
+// dynamic| Tipe dinamis (bisa berubah)   | Bisa apa saja               |
+// =====================================================================|
+
+var greetings = 'Hello Dart!'; // sting
+var myAge = 20; //integers
+
+void main() {
+  var x; //dynamic
+  x = 7;
+  x = 'Dart is great';
+  print(x);
+}
