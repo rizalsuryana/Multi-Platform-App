d4151f75656ff2847bc435770f194098c6c2131d
rizalsuryana <rizal.suryn@gmail.com>
2025-07-16 22:19:55 +0700

    comment

---
+++ Belajar dart/2_Comment.dart
@@ -0,0 +1,15 @@
+// Fungsi main akan menampilkan 2 output
+// Output pertama menampilkan teks dan output kedua menampilkan hasil perkalian pada fungsi [calculate]
+
+void main(List<String> arguments) {
+  // Mencetak Hello Dart! Dart ins great. pada konsol
+  print('Hello Dart! Dart is great');
+
+  // Testing documentation comment with [].
+
+  print('6 * 7 = ${calculate()}');
+}
+
+int calculate() {
+  return 6 * 7;
+}
