4ed31d89deb5bdd42e7e5eba9e7c162577419e9b
rizalsuryana <rizal.suryn@gmail.com>
2025-07-17 13:49:25 +0700

    String

---
+++ Belajar dart/8_String.dart
@@ -0,0 +1,7 @@
+String singleQuote = 'Ini adalah string';
+String doubleQuote = "Ini juga String";
+
+void main() {
+  var vision = 'Front End Developer';
+  print(' "I\'m a ${vision} now !" not only Front-End Web Developer \u2665');
+}
