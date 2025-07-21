d22ad0ecd59bfdc292c327743546d1a3f4fe1c29
rizalsuryana <rizal.suryn@gmail.com>
2025-07-21 22:29:55 +0700

    control flow

---
+++ Belajar dart/ControlFlow/01_IfElse.dart
@@ -0,0 +1,13 @@
+void main() {
+  var isRaining = true;
+
+  print('Prefare before going to office');
+
+  if (isRaining) {
+    print('Oh it\'s raining ... Bring your umbrella');
+  }
+  // if (!isRaining) {
+  //   print('halodo');
+  // }
+  print('Going to the office');
+}
