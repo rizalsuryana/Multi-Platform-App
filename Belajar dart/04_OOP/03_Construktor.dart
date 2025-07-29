1668fb279f8e8a41b274532e1d7c21e0892fc1da
rizalsuryana <rizal.suryn@gmail.com>
2025-07-30 05:38:49 +0700

    Construktor

---
+++ Belajar dart/04_OOP/03_Construktor.dart
@@ -0,0 +1,12 @@
+class Animal {
+  // Kelas tanpa construktor
+  String name = '';
+  int age = 0;
+  double weight = 0;
+
+  Animal(String name, int age, double weight) {
+    this.name = name;
+    this.age = age;
+    this.weight = weight;
+  }
+}
---
+++ Belajar dart/04_OOP/03_Main.dart
@@ -0,0 +1,9 @@
+import '03_Construktor.dart';
+
+void main() {
+  var RizalsCat = Animal();
+
+  RizalsCat.name = 'Mahendro';
+  RizalsCat.age = 2;
+  RizalsCat.weight = 8.8;
+}
